25b79d3ffe555ea47df1f95fcda2968d962e5476
asaru-x <asaru-x@yandex.ru>
2022-04-30 23:23:01 +0300

    fix#2

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,20 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
-    def __init__(self):
-        self.title = models.CharField(max_length=64)
-        self.text = models.TextField()
-        self.added_at = models.DateTimeField(auto_now_add=True)
-        self.likes = models.ManyToManyField(User, related_name='likes_question')
-        self.author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-        self.rating = models.IntegerField(default=0)
+        title = models.CharField(max_length=64)
+        text = models.TextField()
+        added_at = models.DateTimeField(auto_now_add=True)
+        likes = models.ManyToManyField(User, related_name='likes_question')
+        author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+        rating = models.IntegerField(default=0)
 
 class Answer(models.Model):
-    def __init__(self):
-        self.text = models.TextField()
-        self.added_at = models.DateTimeField(auto_now_add=True)
-        self.author = models.CharField(max_length=64)
-        self.question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=64)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
 class QuestionManager(models.Manager):
     def new(self):
