dc7f8d63943453e2979d66e5496c6581fda3a02a
asaru-x <asaru-x@yandex.ru>
2022-05-01 17:08:55 +0300

    fixed

--- short_init.sh
+++ short_init.sh
@@ -1,22 +1,16 @@
-sudo apt update
-sudo apt install python3.5 -y
-sudo apt install python3.5-dev -y
-sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-
 sudo cp etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx reload
 sudo /etc/init.d/mysql start
+
 mysql -u root -e "CREATE DATABASE stepik_web;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON stepik_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
 
-source venv/bin/activate
 gunicorn -D --bind 0.0.0.0:8080 hello:application
 django-admin startproject ask
 cd ask
 python3 manage.py startapp qa
 cd ../etc
-cp settings.py urls.py ask/ask/
-cp models.py views.py ask/qa/
+cp settings.py urls.py ../ask/ask/
+cp models.py views.py ../ask/qa/
 cd ../ask
 gunicorn -D --bind 0.0.0.0:8000 ask.wsgi:application
