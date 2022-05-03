9774684177bb0f686cf7c9e7295a4f27cbe5a91d
asaru-x <asaru-x@yandex.ru>
2022-05-03 21:05:04 +0300

    f

--- short_init.sh
+++ short_init.sh
@@ -1,19 +1,19 @@
-sudo cp etc/nginx.conf /etc/nginx/nginx.conf
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/mysql start
+sudo cp etc/nginx.conf /etc/nginx/nginx.conf &&
+sudo /etc/init.d/nginx restart &&
+sudo /etc/init.d/mysql start &&
 
-mysql -u root -e "CREATE DATABASE stepik_web;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON stepik_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+mysql -u root -e "CREATE DATABASE stepik_web;" &&
+mysql -u root -e "GRANT ALL PRIVILEGES ON stepik_web.* TO 'box'@'localhost' WITH GRANT OPTION;" &&
 
-gunicorn -D --bind 0.0.0.0:8080 hello:application
-django-admin startproject ask
-cd ask
-./manage.py startapp qa
+gunicorn -D --bind 0.0.0.0:8080 hello:application &&
+django-admin startproject ask &&
+cd ask 
+./manage.py startapp qa &&
 cd ../etc
-cp settings.py urls.py ../ask/ask/
+cp settings.py urls.py ../ask/ask/ 
 cp models.py views.py ../ask/qa/
 cp -r templates/ ask/qa/
 cd ../ask
-./manage.py makemigrations
-./manage.py migrate
+./manage.py makemigrations &&
+./manage.py migrate &&
 gunicorn -D --bind 0.0.0.0:8000 ask.wsgi:application
